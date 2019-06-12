35e9730e5905fabc4dae44d88fdb3afafa642bdd
David Omstead <davidomstead@hotmail.com>
2019-06-12 14:44:52 -0700

    Added Create item function, fetch items for store, and DOM mods

--- scripts/api.js
+++ scripts/api.js
@@ -4,15 +4,52 @@ let api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/woodelin';
   let url = `${BASE_URL}/items`;
   function getItems(){
-    // return Promise.resolve('A successful response!');
-    let myPromise = fetch(url)
-      .then(response => {
-        // console.log('HELOOOO');
-        return response.json();
-      });
-      
-    return myPromise;
+    let fetchedPromise = fetch(url)
+    //   .then(response => {
+    //     console.log('HELOOOO');
+    //     return response.json();
+    //   });
+
+    return fetchedPromise;
   }
-  return {getItems, url};
+  function createItem(name){
+    let newItem = JSON.stringify({name});
+    let url = `${BASE_URL}/items`;
+    return fetch(url,{
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: newItem,
+    });
+  }
+  
+  
+  
+  return {getItems,
+    createItem,
+
+
+};
   
 }());
+
+// let create = (function CreateItem(name){
+//   let newItem = JSON.stringify({name});
+//   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/woodelin';
+//   let url = `${BASE_URL}/items`;
+//   fetch(url,{
+//     method:'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: newItem,
+//   });
+//   return {CreateItem};
+// }());
+
+
+
+
+
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,38 @@
 'use strict';
 /* global shoppingList, store, Item */
 // eslint-disable-next-line no-unused-vars
+
+
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
 });
 
-store.items.push(Item.create('apples'));
+// store.items.push(Item.create('apples'));
 
-api.getItems()
-  .then(res => {
-    console.log(res);
-  });
+// api.getItems()
+//   .then(res => {
+//     console.log(res);
+//   });
+
+// api.createItem('pears')
+//   .then(res => res.json())
+//   .then((newItem) => {
+//     return api.getItems();
+//   })
+//   .then(res => res.json())
+//   .then((items) => {
+//     console.log(items);
+//   });
+
+
+
 
 
 // fetch('https://thinkful-list-api.herokuapp.com/ei-student/items')
